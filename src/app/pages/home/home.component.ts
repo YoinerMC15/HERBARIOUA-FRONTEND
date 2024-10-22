1ad772fad3958bd60527fb6aca59b48a3ab63438
YoinerMC <yo.montoya15@gmail.com>
2024-10-22 10:13:36 -0500

    Ajustes para la visualización en el home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import { SidebarComponent } from '../../layout/sidebar/sidebar.component';
-import { TopbarComponent } from '../../layout/topbar/topbar.component';
-//import { FooterComponent } from '../../layout/footer/footer.component';
-
 import { RouterOutlet } from '@angular/router';
+
+// Importación de componentes de layout
+import { TopbarComponent } from '../../layout/topbar/topbar.component';
+import { FooterComponent } from '../../layout/footer/footer.component';
+import { SidebarComponent } from '../../layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +13,13 @@ import { RouterOutlet } from '@angular/router';
   imports: [
     CommonModule,
     TopbarComponent,
+    FooterComponent,
+    SidebarComponent,
     RouterOutlet
   ],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  containerClass: string = '';  // Definimos la propiedad 'containerClass' con un valor inicial
+}
